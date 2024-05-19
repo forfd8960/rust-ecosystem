9922da83a1bf0bed82fa9bd3b499f10acf0b68e7
forfd8960 <forfd8960@gmail.com>
2024-05-19 11:36:55 +0800

    feature: Integrate opentelemetry with tracing_subcribier

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 ## Tracing
 
 ![tracing](tracing.png)
+
+## OpenTelemetry
+
+```sh
+cargo add opentelemetry-otlp --features tonic
+
+cargo add opentelemetry
+
+cargo add opentelemetry_sdk --features rt-tokio
+
+cargo add tracing-opentelemetry
+```
--- examples/tracing.rs
+++ examples/tracing.rs
@@ -2,13 +2,27 @@ use std::time::Duration;
 
 use axum::{routing::get, Router};
 
-use tokio::time::{sleep, Instant};
+use opentelemetry::KeyValue;
+use opentelemetry_otlp::WithExportConfig;
+use opentelemetry_sdk::{
+    runtime,
+    trace::{self, RandomIdGenerator, Tracer},
+    Resource,
+};
+
+use tokio::{
+    join,
+    time::{sleep, Instant},
+};
 use tracing::level_filters::LevelFilter;
 use tracing::{info, instrument, warn};
 
-use tracing_subscriber::fmt::{self, format::FmtSpan};
-use tracing_subscriber::prelude::*;
-use tracing_subscriber::Layer;
+use tracing_subscriber::{
+    fmt::{self, format::FmtSpan},
+    layer::SubscriberExt,
+    util::SubscriberInitExt,
+    Layer,
+};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -28,9 +42,13 @@ async fn main() -> anyhow::Result<()> {
         .pretty()
         .with_filter(LevelFilter::WARN);
 
+    // let tracer = init_tracer()?;
+    // let opentelemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+
     tracing_subscriber::registry()
         .with(console)
         .with(file)
+        // .with(opentelemetry)
         .init();
 
     let addr = "0.0.0.0:8080";
@@ -54,7 +72,52 @@ async fn index_handler() -> &'static str {
 async fn long_runing() -> &'static str {
     let start = Instant::now();
     sleep(Duration::from_millis(100)).await;
+
+    let t1 = task1();
+    let t2 = task2();
+    let t3 = task3();
+    join!(t1, t2, t3);
+
     let elapsed = start.elapsed().to_owned().as_millis();
     warn!(app.task_duration = elapsed, "long running task~");
     "Serve APP"
+}
+
+#[instrument]
+async fn task1() {
+    sleep(Duration::from_millis(10)).await;
+}
+
+#[instrument]
+async fn task2() {
+    sleep(Duration::from_millis(20)).await;
+}
+
+#[instrument]
+async fn task3() {
+    sleep(Duration::from_millis(30)).await;
+}
+
+// #[allow_deadcode]
+fn init_tracer() -> anyhow::Result<Tracer> {
+    let tracer = opentelemetry_otlp::new_pipeline()
+        .tracing()
+        .with_exporter(
+            opentelemetry_otlp::new_exporter()
+                .tonic()
+                .with_endpoint("http://localhost:4317"),
+        )
+        .with_trace_config(
+            trace::config()
+                .with_id_generator(RandomIdGenerator::default())
+                .with_max_events_per_span(32)
+                .with_max_attributes_per_span(64)
+                .with_resource(Resource::new(vec![KeyValue::new(
+                    "service.name",
+                    "axum-tracing",
+                )])),
+        )
+        .install_batch(runtime::Tokio)?;
+
+    anyhow::Ok(tracer)
 }
